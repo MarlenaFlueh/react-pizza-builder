42e1b991705a9f27a3d5e9c01062444243f6d338
MarlenaFlueh <mf@esyoil.com>
2018-07-18 18:03:34 +0200

    add navbar and logo (exampöe)

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Logo from "../Logo/Logo";
+
+const Navbar = () => (
+  <header>
+    <div>
+      <Logo />
+    </div>
+    <nav />
+  </header>
+);
+
+export default Navbar;
