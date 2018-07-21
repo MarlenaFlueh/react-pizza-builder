354463f8f4bca7d7c054c444cec95a161b31ee9b
MarlenaFlueh <mf@esyoil.com>
2018-07-21 23:57:17 +0200

    add ingredients

--- src/components/PizzaBuilder/IngredientPosition.js
+++ src/components/PizzaBuilder/IngredientPosition.js
@@ -1,51 +1,73 @@
-export const pepperOne = [
+export const redPepper = [
   { col: 3, row: 1 },
-  { col: 4, row: 2 },
-  { col: 2, row: 5 }
-];
-
-export const pepperTwo = [{ col: 1, row: 2 }, { col: 5, row: 4 }];
-
-export const pepperoni = [
-  { col: 2, row: 1 },
-  { col: 4, row: 1 },
   { col: 1, row: 2 },
-  { col: 1, row: 4 },
-  { col: 2, row: 4 },
+  { col: 4, row: 2 },
+  { col: 3, row: 4 },
   { col: 4, row: 4 }
 ];
 
-export const mozerella = [
-  { col: 3, row: 1 },
-  { col: 4, row: 2 },
+export const greenPepper = [
+  { col: 5, row: 2 },
+  { col: 2, row: 2 },
   { col: 1, row: 3 },
   { col: 3, row: 3 },
   { col: 2, row: 4 }
 ];
 
+export const pepperoni = [
+  { col: 2, row: 1 },
+  { col: 4, row: 1 },
+  { col: 1, row: 2 },
+  { col: 3, row: 2 },
+  { col: 2, row: 3 },
+  { col: 1, row: 3 },
+  { col: 4, row: 3 },
+  { col: 3, row: 4 }
+];
+
+export const mozerella = [
+  { col: 2, row: 1 },
+  { col: 4, row: 1 },
+  { col: 1, row: 2 },
+  { col: 3, row: 2 },
+  { col: 1, row: 3 },
+  { col: 5, row: 2 },
+  { col: 4, row: 4 },
+  { col: 3, row: 3 },
+  { col: 4, row: 3 },
+  { col: 2, row: 4 }
+];
+
 export const mushroom = [
   { col: 4, row: 1 },
+  { col: 2, row: 1 },
   { col: 3, row: 2 },
   { col: 5, row: 2 },
   { col: 2, row: 3 },
   { col: 4, row: 3 },
-  { col: 2, row: 1 }
+  { col: 3, row: 4 }
 ];
 
 export const tomato = [
+  { col: 3, row: 1 },
   { col: 2, row: 2 },
+  { col: 4, row: 2 },
   { col: 3, row: 3 },
-  { col: 4, row: 3 },
+  { col: 1, row: 3 },
   { col: 5, row: 3 },
-  { col: 5, row: 4 },
-  { col: 3, row: 5 }
+  { col: 2, row: 4 },
+  { col: 4, row: 4 }
 ];
 
 export const olive = [
   { col: 2, row: 1 },
+  { col: 4, row: 1 },
+  { col: 1, row: 2 },
   { col: 2, row: 2 },
+  { col: 2, row: 3 },
+  { col: 4, row: 3 },
+  { col: 5, row: 3 },
   { col: 5, row: 2 },
-  { col: 1, row: 4 },
-  { col: 3, row: 4 },
-  { col: 4, row: 5 }
+  { col: 2, row: 4 },
+  { col: 4, row: 4 }
 ];
