92760652c58894c118b908559536cb2d9bc5e73a
MarlenaFlueh <mf@esyoil.com>
2018-07-27 14:20:20 +0200

    create reducer files and outsource store

---
+++ src/reducers/ingsReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  ings: null
+};
+
+const ingsReducer = (state = initialState) => {
+  switch (action.type) {
+    case "":
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default ingsReducer;
